86fbfbac0309c11c63e60a594ce768fd1d09761a
kennsdspa <caiosartorellofra@gmail.com>
2026-02-19 16:37:44 -0300

    banco att

--- conectarbanco.php
+++ conectarbanco.php
@@ -2,9 +2,9 @@
 
 $config = array(
     'db_host' => 'localhost',
-    'db_user' => 'root',
-    'db_pass' => '',
-    'db_name' => 'subway-pay'
+    'db_user' => 'u114777039_root',
+    'db_pass' => 'Subway71@',
+    'db_name' => 'u114777039_subwaypay'
 );
 
 ?>
